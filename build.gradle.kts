605b0d3b61f8cef594d458ae3196edd7fd05537d
Markus Wirtz <info@markuswirtz.net>
2025-03-05 01:25:05 +0100

    Datensicherung und Änderung des Projektnamens

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "demo"
+            packageName = "MyMiniNotes"
             packageVersion = "1.0.0"
         }
     }
